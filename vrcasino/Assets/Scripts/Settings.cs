c8c97af3c4e13253363a96430b7dfc2c81eb7265
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-10-14 15:43:16 +0300

    Fixed hands

---
+++ vrcasino/Assets/Scripts/Settings.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+    private bool smoothTurnEnabled;
+    [SerializeField]
+    private double movementSpeed;
+    [SerializeField]
+    private double audioVolume;
+    [SerializeField]
+    private ActionBasedControllerManager actionManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
