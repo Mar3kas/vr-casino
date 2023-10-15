047efce7695a5cbfe8de58ce431e18a23e002188
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2023-10-15 18:09:20 +0300

    Hand menu

---
+++ vrcasino/Assets/Scripts/HandMenu.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandMenu : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionAsset inputActions;
+    [SerializeField]
+    private Canvas handMenuCanvas;
+    [SerializeField]
+    private Canvas settingsMenuCanvas;
+    private InputAction menuInputAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuInputAction = inputActions.FindActionMap("XRI LeftHand").FindAction("Menu");
+        menuInputAction.Enable();
+        menuInputAction.performed += toggleMenu;
+        disableHandMenuCanvas();
+        disableSettingsMenuCanvas();
+    }
+
+    void enableHandMenuCanvas()
+    {
+        handMenuCanvas.enabled = true;
+    }
+
+    void disableHandMenuCanvas()
+    {
+        handMenuCanvas.enabled = false;
+    }
+
+    void enableSettingsMenuCanvas()
+    {
+        settingsMenuCanvas.enabled = true;
+    }
+
+    void disableSettingsMenuCanvas()
+    {
+        settingsMenuCanvas.enabled = false;
+    }
+
+    void toggleMenu(InputAction.CallbackContext context)
+    {
+        if (!handMenuCanvas.enabled && !settingsMenuCanvas.enabled)
+        {
+            enableHandMenuCanvas();
+        }
+        else if (handMenuCanvas.enabled && !settingsMenuCanvas.enabled)
+        {
+            disableHandMenuCanvas();
+        }
+        else if (!handMenuCanvas.enabled && settingsMenuCanvas.enabled)
+        {
+            disableSettingsMenuCanvas();
+        }
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void enableSettingsMenu()
+    {
+        disableHandMenuCanvas();
+        enableSettingsMenuCanvas();
+    }
+
+    public void backToHandMenu()
+    {
+        disableSettingsMenuCanvas();
+        enableHandMenuCanvas();
+    }
+
+    public void applySettings()
+    {
+        //Logic to apply settings
+        disableSettingsMenuCanvas();
+        enableHandMenuCanvas();
+    }
+}
--- vrcasino/Assets/Scripts/Settings.cs
+++ vrcasino/Assets/Scripts/Settings.cs
@@ -11,8 +11,6 @@ public class Settings : MonoBehaviour
     private double movementSpeed;
     [SerializeField]
     private double audioVolume;
-    [SerializeField]
-    private ActionBasedControllerManager actionManager;
 
     // Start is called before the first frame update
     void Start()
